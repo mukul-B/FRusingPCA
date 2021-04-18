c78c72e3ee67eb1efe20894d155b38910099abdf
mukul <mukulbadhan@gmail.com>
2021-04-17 18:47:42 -0700

    intial

---
+++ cmake-build-debug/CMakeFiles/FRusingPCA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FRusingPCA.dir/main.cpp.obj"
+  "FRusingPCA.exe"
+  "FRusingPCA.exe.manifest"
+  "FRusingPCA.pdb"
+  "libFRusingPCA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FRusingPCA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
